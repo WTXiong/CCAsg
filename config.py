2502ceec91955a73fd03ca286b73463596edb9fa
YEOH XIONG KEAT <89540215+WTXiong@users.noreply.github.com>
2022-09-27 21:54:41 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "database-1.cvy7wbz5p7ok.us-east-1.rds.amazonaws.com"
+customhost = "database-1.c24yyu4zym88.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "tangkinseng-bucket"
+custombucket = "yeohxiongkeat-busket"
 customregion = "us-east-1"
